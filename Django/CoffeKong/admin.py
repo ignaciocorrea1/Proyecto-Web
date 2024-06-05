f2373c2b2c066939d81ea8e1ad0cec3094b76e72
ignaciocorrea1 <ig.correar@duocuc.cl>
2024-06-04 22:10:18 -0400

    Modelo v1
    Modelo base

--- Django/CoffeKong/admin.py
+++ Django/CoffeKong/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import cliente, vendedor, tarjeta, tipoProducto, producto, pedido, detallePedido
 
 # Register your models here.
+admin.site.register(cliente)
+admin.site.register(vendedor)
+admin.site.register(tarjeta)
+admin.site.register(tipoProducto)
+admin.site.register(producto)
+admin.site.register(pedido)
+admin.site.register(detallePedido)
--- Django/CoffeKong/models.py
+++ Django/CoffeKong/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
 # Create your models here.
+    
+""" Cliente """
+class cliente(models.Model):
+    run = models.CharField(max_length=12, primary_key=True, db_column="run")
+    nombres = models.CharField(max_length=80)
+    apaterno = models.CharField(max_length=30)
+    amaterno = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=10)
+    correo = models.CharField(max_length=50)
+    contrasenia = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str("Cliente: " + self.run)
+
+""" Vendedor """
+class vendedor(models.Model):
+    run = models.CharField(max_length=12, primary_key=True, db_column="run")
+    nombres = models.CharField(max_length=80)
+    apaterno = models.CharField(max_length=30)
+    amaterno = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=10)
+    correo = models.CharField(max_length=50)
+    contrasenia = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str("Vendedor: " + self.run)
+
+""" Tarjeta """
+class tarjeta(models.Model):
+    nro_tarjeta = models.CharField(primary_key=True, db_column="nroTarjeta", max_length=30)
+    run = models.ForeignKey("cliente", on_delete=models.CASCADE, db_column="run")
+
+    def __str__(self):
+        return str(self.nro_tarjeta)
+
+""" Tipo de producto """
+class tipoProducto(models.Model):
+    id_tipoproducto = models.CharField(max_length=2, primary_key=True, db_column="idTipoProducto")
+    descripcion = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.descripcion)
+
+""" Producto """
+class producto(models.Model):
+    id_producto = models.IntegerField(primary_key=True, db_column="idProducto")
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=300)
+    precio = models.IntegerField()
+    imagen = models.CharField(max_length=100)
+    tipo = models.ForeignKey("tipoProducto", on_delete=models.CASCADE, db_column="idTipoProducto")
+
+    def __str__(self):
+        return str(self.nombre)
+
+""" Pedido """
+class pedido(models.Model):
+    id_pedido = models.IntegerField(primary_key=True, db_column="idPedido")
+    fecha = models.DateField()
+
+    def __str__(self):
+        return str(self.id_pedido)
+
+""" Detalle pedido """
+class detallePedido(models.Model):
+    id_pedido = models.ForeignKey("pedido", on_delete=models.CASCADE, db_column="idPedido")
+    id_producto = models.ForeignKey("producto", on_delete=models.CASCADE, db_column="idProducto")
+    total = models.IntegerField()
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return str("Detalle del pedido" + self.id_pedido)
